6c2383eb0f12910c9333918d4b7f16d23e008664
yene <yene@cocoadojo.com>
2015-04-08 15:56:05 +0200

    setup menu item and the duration between the button presses
    As mentioned on [stackoverflow](http://stackoverflow.com/a/26198173/279890) NSStatusItem has since 10.10 a property button which returns you a NSStatusBarButton to configure.

--- Simple Duration/AppDelegate.swift
+++ Simple Duration/AppDelegate.swift
@@ -6,22 +6,58 @@
 //  Copyright (c) 2015 Yannick Weiss. All rights reserved.
 //
 
+// 🐱🐱🐱🐱🐱🐱🐱🐱🐱🐱🐱🐱🐱🐱🐱🐱🐱🐱🐱🐱🐱🐱🐱🐱🐱🐱
+
 import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-  @IBOutlet weak var window: NSWindow!
-
+  var statusItem: NSStatusItem?
+  var button: NSStatusBarButton?
+  var started = false
+  var startTime : NSDate?
 
   func applicationDidFinishLaunching(aNotification: NSNotification) {
     // Insert code here to initialize your application
   }
 
   func applicationWillTerminate(aNotification: NSNotification) {
-    // Insert code here to tear down your application
+  }
+  
+  override func awakeFromNib() {
+
+    self.statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-1.0)
+    self.button = self.statusItem?.button
+    self.button?.title = "Start"
+    self.button?.action = "start:"
+    self.button?.target = self
+  }
+  
+  @IBAction func start(sender : AnyObject) {
+    println("start pressed")
+    
+    button?.title = "Stop"
+    button?.action = "stop:"
+    
+    startTime = NSDate();
   }
 
+  @IBAction func stop(sender : AnyObject) {
+    println("stop pressed")
+    
+    button?.title = "Start"
+    button?.action = "start:"
+    
+    var duration: NSTimeInterval! = startTime?.timeIntervalSinceNow
+    duration = duration * -1
+    
+    let min: Double = duration / 60    // divide two longs, truncates
+    let sec: Double = duration % 60    // remainder of long divide
+    let str = NSString(format: "%02d:%02d", Int(min), Int(sec))
+    
+    NSLog("duration %@", str)
+  }
 
 }
 
