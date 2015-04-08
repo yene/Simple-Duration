6f8801bec88450e9a0da2b15810b277540b435cb
yene <yene@cocoadojo.com>
2015-04-08 11:38:30 +0200

    Initial Commit

---
+++ Simple Duration/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Simple Duration
+//
+//  Created by Yannick Weiss on 08/04/15.
+//  Copyright (c) 2015 Yannick Weiss. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
