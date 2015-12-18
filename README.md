7a19eb83b447645fb07b6b1d33f6e422eb860fb8
yene <yene@slowmouse.ch>
2015-12-18 19:25:53 +0100

    added time rounding

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 A very simple Menu Bar App that copies your working period to clipboard so you can paste it into Numbers/Word.
 ![screenshot](screenshot.png)
 
+# Warning this app rounds minutes up or down to the next 15 minutes.
+
 # Date Format
-The content inside the clipboard is formatted `08/04/2015  00h 00m`.
+The content inside the clipboard is formatted `08.04.2015	08:00	12:00`. But you can easily modify it.
 
 # Status Bar in 10.10
 As mentioned on [stackoverflow](http://stackoverflow.com/a/26198173/279890) NSStatusItem has since 10.10 a property `button` which returns you a NSStatusBarButton to configure.
@@ -12,3 +14,4 @@ I used a simple script to cut an image into the required icon formats. https://g
 
 # TODO
 - [ ] Round the time nearest 15 minutes
+- [ ] Holding mouse pressed should quit the app.
--- Simple Duration/AppDelegate.swift
+++ Simple Duration/AppDelegate.swift
@@ -12,72 +12,91 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-  var statusItem: NSStatusItem?
-  var button: NSStatusBarButton?
-  var started = false
-  var startTime : NSDate = NSDate()
+	var statusItem: NSStatusItem?
+	var button: NSStatusBarButton?
+	var started = false
+	var startTime : NSDate = NSDate()
 
-  func applicationDidFinishLaunching(aNotification: NSNotification) {
-    // Insert code here to initialize your application
-  }
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
 
-  func applicationWillTerminate(aNotification: NSNotification) {
-  }
-  
-  override func awakeFromNib() {
+	func applicationWillTerminate(aNotification: NSNotification) {
+	}
 
-    // Workaround for linker bug  http://stackoverflow.com/a/24026327/279890
-    let NSVariableStatusItemLength: CGFloat = -1.0;
+	override func awakeFromNib() {
 
-    self.statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
-    self.button = self.statusItem?.button
-    self.button?.title = "Start"
-    self.button?.action = "start:"
-    self.button?.target = self
-  }
-  
-  func start(sender : AnyObject) {
-    button?.title = "Stop"
-    button?.action = "stop:"
-    startTime = NSDate();
-  }
+		// Workaround for linker bug  http://stackoverflow.com/a/24026327/279890
+		let NSVariableStatusItemLength: CGFloat = -1.0;
 
-  func stop(sender : AnyObject) {
-    button?.enabled = false
-		
-    let dateFormatter = NSDateFormatter()
-    dateFormatter.dateFormat = "dd.MM.yyyy" //"yyyy-MM-dd 'at' HH:mm"
-    let today = dateFormatter.stringFromDate(NSDate())
+		self.statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
+		self.button = self.statusItem?.button
+		self.button?.title = "Start"
+		self.button?.action = "start:"
+		self.button?.target = self
+	}
+
+	func start(sender : AnyObject) {
+		button?.title = "Stop"
+		button?.action = "stop:"
+		startTime = NSDate()
+	}
+
+	func stop(sender : AnyObject) {
+		button?.enabled = false
+
+		let dateFormatter = NSDateFormatter()
+		dateFormatter.dateFormat = "dd.MM.yyyy" //"yyyy-MM-dd 'at' HH:mm"
+		let today = dateFormatter.stringFromDate(NSDate())
 
 		var startHour = NSCalendar.currentCalendar().component(.Hour, fromDate: startTime)
 		var startMinute = NSCalendar.currentCalendar().component(.Hour, fromDate: startTime)
+		(startHour, startMinute) = roundTime(startHour, minutes: startMinute)
+		
 		
 		var currentHour = NSCalendar.currentCalendar().component(.Hour, fromDate: NSDate())
 		var currentMinute = NSCalendar.currentCalendar().component(.Minute, fromDate: NSDate())
-				
-    let str = String(format: "%@\t%02d:%02d\t%02d:%02d", today, startHour, startMinute, currentHour, currentMinute)
-    
-    let pboard = NSPasteboard.generalPasteboard()
-    pboard.declareTypes([NSStringPboardType], owner: nil)
-    pboard.setString(str, forType: NSStringPboardType)
-    
-    button?.title = "Copied to Clipboard"
-    let dispatchTime: dispatch_time_t = dispatch_time(DISPATCH_TIME_NOW, Int64(0.5 * Double(NSEC_PER_SEC)))
-    dispatch_after(dispatchTime, dispatch_get_main_queue(), {
-      self.button?.enabled = true
-      self.button?.title = "Start"
-      self.button?.action = "start:"
-    })
-    
-  }
-	
+		(currentHour, currentMinute) = roundTime(currentHour, minutes: currentMinute)
+
+
+		let str = String(format: "%@\t%02d:%02d\t%02d:%02d", today, startHour, startMinute, currentHour, currentMinute)
+
+		let pboard = NSPasteboard.generalPasteboard()
+		pboard.declareTypes([NSStringPboardType], owner: nil)
+		pboard.setString(str, forType: NSStringPboardType)
+
+		button?.title = "Copied to Clipboard"
+		let dispatchTime: dispatch_time_t = dispatch_time(DISPATCH_TIME_NOW, Int64(0.5 * Double(NSEC_PER_SEC)))
+		dispatch_after(dispatchTime, dispatch_get_main_queue(), {
+				self.button?.enabled = true
+				self.button?.title = "Start"
+				self.button?.action = "start:"
+			})
+
+	}
+
 	func timeSince(date: NSDate) -> String {
 		var duration: NSTimeInterval! = date.timeIntervalSinceNow
 		duration = duration * -1
-		
-		let hrs: Double = duration / (60*60)
+
+		let hrs: Double = duration / (60 * 60)
 		let min: Double = (duration / 60) % 60
 		return String(format: "%02dh %02dm", Int(hrs), Int(min))
+	}
+
+	func roundTime(hours: Int, minutes: Int) -> (Int, Int) {
+		switch (minutes) {
+		case 0..<7:
+			return(hours, 0)
+		case 7..<22:
+			return(hours, 15)
+		case 22..<37:
+			return(hours, 30)
+		case 37..<52:
+			return(hours, 45)
+		default:
+			return(hours + 1, 0)
+		}
 	}
 
 }
