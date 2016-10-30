ebed46b39d75df3c194cb7b5e856b92a19f60102
Yannick Weiss <contact@yannickweiss.com>
2016-10-30 14:40:28 +0100

    Migrated to Swift 3 and added example sheet

--- Simple Duration/AppDelegate.swift
+++ Simple Duration/AppDelegate.swift
@@ -16,13 +16,13 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 	var statusItem: NSStatusItem?
 	var button: NSStatusBarButton?
 	var started = false
-	var startTime : NSDate = NSDate()
+	var startTime : Date = Date()
 
-	func applicationDidFinishLaunching(aNotification: NSNotification) {
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
 		// Insert code here to initialize your application
 	}
 
-	func applicationWillTerminate(aNotification: NSNotification) {
+	func applicationWillTerminate(_ aNotification: Notification) {
 	}
 
 	override func awakeFromNib() {
@@ -30,54 +30,54 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 		// Workaround for linker bug  http://stackoverflow.com/a/24026327/279890
 		let NSVariableStatusItemLength: CGFloat = -1.0;
 
-		self.statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
+		self.statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
 		self.button = self.statusItem?.button
 		self.button?.title = "Start"
 		self.button?.action = #selector(AppDelegate.start(_:))
 		self.button?.target = self
 	}
 
-	func start(sender : AnyObject) {
+	func start(_ sender : AnyObject) {
 		button?.title = "Stop"
 		button?.action = #selector(AppDelegate.stop(_:))
-		startTime = NSDate()
+		startTime = Date()
 	}
 
-	func stop(sender : AnyObject) {
-		button?.enabled = false
+	func stop(_ sender : AnyObject) {
+		button?.isEnabled = false
 
-		let dateFormatter = NSDateFormatter()
+		let dateFormatter = DateFormatter()
 		dateFormatter.dateFormat = "dd.MM.yyyy" //"yyyy-MM-dd 'at' HH:mm"
-		let today = dateFormatter.stringFromDate(NSDate())
+		let today = dateFormatter.string(from: Date())
 
-		let startHour = NSCalendar.currentCalendar().component(.Hour, fromDate: startTime)
-		let startMinute = NSCalendar.currentCalendar().component(.Minute, fromDate: startTime)
+		let startHour = (Calendar.current as NSCalendar).component(.hour, from: startTime)
+		let startMinute = (Calendar.current as NSCalendar).component(.minute, from: startTime)
 		
-		let currentHour = NSCalendar.currentCalendar().component(.Hour, fromDate: NSDate())
-		let currentMinute = NSCalendar.currentCalendar().component(.Minute, fromDate: NSDate())
+		let currentHour = (Calendar.current as NSCalendar).component(.hour, from: Date())
+		let currentMinute = (Calendar.current as NSCalendar).component(.minute, from: Date())
 
 		let str = String(format: "%@\t%02d:%02d\t%02d:%02d", today, startHour, startMinute, currentHour, currentMinute)
 
-		let pboard = NSPasteboard.generalPasteboard()
+		let pboard = NSPasteboard.general()
 		pboard.declareTypes([NSStringPboardType], owner: nil)
 		pboard.setString(str, forType: NSStringPboardType)
 
 		button?.title = "Copied to Clipboard"
-		let dispatchTime: dispatch_time_t = dispatch_time(DISPATCH_TIME_NOW, Int64(0.5 * Double(NSEC_PER_SEC)))
-		dispatch_after(dispatchTime, dispatch_get_main_queue(), {
-				self.button?.enabled = true
+		let dispatchTime: DispatchTime = DispatchTime.now() + Double(Int64(0.5 * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)
+		DispatchQueue.main.asyncAfter(deadline: dispatchTime, execute: {
+				self.button?.isEnabled = true
 				self.button?.title = "Start"
 				self.button?.action = #selector(AppDelegate.start(_:))
 			})
 
 	}
 
-	func timeSince(date: NSDate) -> String {
-		var duration: NSTimeInterval! = date.timeIntervalSinceNow
+	func timeSince(_ date: Date) -> String {
+		var duration: TimeInterval! = date.timeIntervalSinceNow
 		duration = duration * -1
 
 		let hrs: Double = duration / (60 * 60)
-		let min: Double = (duration / 60) % 60
+		let min: Double = (duration / 60).truncatingRemainder(dividingBy: 60)
 		return String(format: "%02dh %02dm", Int(hrs), Int(min))
 	}
 	
@@ -101,20 +101,20 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 		self.statusItem!.menu = nil
 	}
 	
-	func quit(sender: AnyObject) {
-		NSApplication.sharedApplication().terminate(self)
+	func quit(_ sender: AnyObject) {
+		NSApplication.shared().terminate(self)
 	}
     
-    func lastWakeup(sender: AnyObject) {
+    func lastWakeup(_ sender: AnyObject) {
         // http://blog.nottoobadsoftware.com/swiftshell/how-to-use-swift-for-shell-scripting/
         let cmd = "pmset -g log | grep \" Wake\" | tail -n 1 | awk '{if($2 != \"since\") print $2; else print $6; }' | pbcopy"
-        NSTask.launchedTaskWithLaunchPath("/bin/bash", arguments:["-c", cmd]).waitUntilExit()
+        Process.launchedProcess(launchPath: "/bin/bash", arguments:["-c", cmd]).waitUntilExit()
     }
 
 }
 
 extension NSStatusBarButton {
-	override public func rightMouseDown(event: NSEvent) {
+	override open func rightMouseDown(with event: NSEvent) {
 		self.target?.addMenu()
 	}
 }
